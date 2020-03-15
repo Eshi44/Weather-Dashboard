2dcce426727185f69dba542e37a4ffdb136fd805
Eshi44 <e.hameliervolino@gmail.com>
2020-03-15 18:39:13 -0400

    added inital pseudocode for framework and added the bootstrap relative link

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Hello World");
+//use bootstrap relative link
+//create a nav bar
+//create a container
+//create a row with a fairly large height
+//add two col(col-4 and col-8)
+//first col has 2 divs - a search for city button 
+//second col has a div card with title and 4 other divs
+//then add another div with title and 5 small cards with padding
+
