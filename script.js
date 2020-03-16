7a6d85cb5f85eb61531720c5377b8598d1304178
Eshi44 <e.hameliervolino@gmail.com>
2020-03-16 17:46:18 -0400

    finished the five day forcast input and edited css so icon would display correctly

--- script.js
+++ script.js
@@ -65,47 +65,70 @@ $(document).ready(function() {
 
                    var fiveDayWeekDates = [dateOne,dateTwo,dateThree,dateFour,dateFive ];
 
-                   console.log(fiveDayWeekDates);
+                  // console.log(fiveDayWeekDates);
 
                    var dateTitle = document.getElementsByClassName("datetitle");
                 
                    for (var i=0; i<fiveDayWeekDates.length; i++) {
                       dateTitle[i].innerHTML=fiveDayWeekDates[i];
-                      console.log(fiveDayWeekDates.length);
+                    //  console.log(fiveDayWeekDates.length);
                    }
 
-                   });
 
 
-            $.ajax({
-                url: 'http://api.openweathermap.org/data/2.5/forecast/?q=' + city +"&units=imperial" +
-                "&APIkey=d1817ee7c3ae109bbf6ea614ec59a4cb" + "&cnt=5",
-                method: "GET",
-             }).then(function(data) {
-                    console.log(data);
+                   var array = [data.list[0].main.temp, data.list[1].main.temp, data.list[2].main.temp, 
+                   data.list[3].main.temp, data.list[4].main.temp];
 
-                     var array = [data.list[0].main.temp, data.list[1].main.temp, data.list[2].main.temp, 
-                     data.list[3].main.temp, data.list[4].main.temp];
- 
-                     //console.log(array);
-                     
-                     var temperatureForecast = $(".tempFive");
-                
-                 for (var i=0; i<array.length; i++) {
-                    temperatureForecast[i].append("Temp:" + array[i] + "°F");
-                    console.log(array.length);
-                    //console.log(temperatureForecast);
+                   //console.log(array);
+                   
+                   var temperatureForecast = $(".tempFive");
+              
+               for (var i=0; i<array.length; i++) {
+                  temperatureForecast[i].append("Temp:" + array[i] + "°F");
+                 // console.log(array.length);
+                  //console.log(temperatureForecast);
 
 
-                 }
+               }
+
+                var humidArray = [data.list[0].main.humidity, data.list[1].main.humidity, data.list[2].main.humidity, 
+                data.list[3].main.humidity, data.list[4].main.humidity];
+
+                var humidityForecast = $(".humidFive");
+
+                for (var i=0; i<humidArray.length; i++) {
+                    humidityForecast[i].append("Humidity:" + humidArray[i] + "%");
+                }
+
+
+                    var weatherIcon1 = $("#weatherIcon1");
+                    var iconurl1 = "http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png";
+                    weatherIcon1.append();
+                    $('#wicon1').attr('src', iconurl1);
+
+
+                    var weatherIcon2 = $("#weatherIcon2");
+                    var iconurl2 = "http://openweathermap.org/img/w/" + data.list[1].weather[0].icon + ".png";
+                    weatherIcon2.append();
+                    $('#wicon2').attr('src', iconurl2);
+
+
+                    var weatherIcon3 = $("#weatherIcon3");
+                    var iconurl3 = "http://openweathermap.org/img/w/" + data.list[2].weather[0].icon + ".png";
+                    weatherIcon3.append();
+                    $('#wicon3').attr('src', iconurl3);
 
 
 
+                    var weatherIcon4 = $("#weatherIcon4");
+                    var iconurl4 = "http://openweathermap.org/img/w/" + data.list[3].weather[0].icon + ".png";
+                    weatherIcon4.append();
+                    $('#wicon4').attr('src', iconurl4);
 
-
-
-
-
+                    var weatherIcon5 = $("#weatherIcon5");
+                    var iconurl5 = "http://openweathermap.org/img/w/" + data.list[4].weather[0].icon + ".png";
+                    weatherIcon5.append();
+                    $('#wicon5').attr('src', iconurl5);
 
 
                 });
