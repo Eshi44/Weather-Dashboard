d9b66291423c0b8b2fd06ba3dd58a4cd2dfe7f21
Eshi44 <e.hameliervolino@gmail.com>
2020-03-16 08:24:04 -0400

    added ajax method, moment.js realtive link, and appended city name and date to the DOM

--- script.js
+++ script.js
@@ -8,3 +8,39 @@ console.log("Hello World");
 //second col has a div card with title and 4 other divs
 //then add another div with title and 5 small cards with padding
 
+//add ready method
+$(document).ready(function() {
+
+   
+
+//clikc funtion for allowing city info to populate
+    $('#btn').click(function() {
+
+            
+        var city = $("#city").val();
+
+        //console.log(city);
+
+        $.ajax({
+            url: 'http://api.openweathermap.org/data/2.5/weather?q=' +city + "&units=imperial" +
+            "&APIkey=d1817ee7c3ae109bbf6ea614ec59a4cb",
+            method: "GET",
+         }).then(function(data) {
+                console.log(data);
+                console.log(data.clouds);
+
+                var date = moment().format('L');
+                var cardTitle = $(".card-title");
+                cardTitle.append(city +" ("+ date +")");
+
+
+            });
+
+        
+
+    });
+
+
+
+
+});
