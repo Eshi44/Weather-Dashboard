3e90534a1dbb1fdeaf2b5a64397e16cc4ad4424a
Eshi44 <e.hameliervolino@gmail.com>
2020-03-17 17:39:18 -0400

    added comments and touched up code. Edited relative js and css paths in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,12 @@
 //console.log("Hello World");
-//use bootstrap relative link
-//create a nav bar
-//create a container
-//create a row with a fairly large height
-//add two col(col-4 and col-8)
-//first col has 2 divs - a search for city button
-//second col has a div card with title and 4 other divs
-//then add another div with title and 5 small cards with padding
 
-//add ready method
-
+//define city variable
 var city;
-
+//add ready method
 $(document).ready(function() {
   var savedCity = localStorage.getItem("city");
   console.log("savedCity" + savedCity);
-
+  //if city is not null, pull from local storage and run setWeather function
   if (savedCity != null) {
     city = savedCity;
     // city=JSON.parse(city);
@@ -25,30 +16,30 @@ $(document).ready(function() {
     // console.log(city);
   }
 
-  //clikc funtion for allowing city info to populate
+  //click funtion for allowing city info to populate
   $("#btn").click(function() {
     // console.log(savedCity + " in button");
     // console.log("is saved city true? " + savedCity != null);
     // console.log("is saved city found in button?");
 
+    //variable condition to pull from storage if not null and reload page to clear previous city value
     var savedCity = localStorage.getItem("city");
     if (savedCity != null) {
-        city = $("#city").val();
-        localStorage.setItem("city", JSON.stringify(city));
-        location.reload(true);
+      city = $("#city").val();
+      localStorage.setItem("city", JSON.stringify(city));
+      location.reload(true);
     }
     city = $("#city").val();
     localStorage.setItem("city", JSON.stringify(city));
     setWeather();
   });
 });
-
-
+//set weather function will be called in the event listener
 function setWeather() {
-  
-    city=JSON.parse(city);
+  //change city from string to object
+  city = JSON.parse(city);
 
-  console.log("setting weather to city " + city);
+  // console.log("setting weather to city " + city);
 
   var unlist = $(".inputCityName")
     .append("<ul>")
@@ -57,7 +48,7 @@ function setWeather() {
   $(".list-group-item").append(city);
 
   // console.log(list);
-
+  //ajax for current day weather
   $.ajax({
     url:
       "http://api.openweathermap.org/data/2.5/weather/?q=" +
@@ -87,7 +78,7 @@ function setWeather() {
     var wind = $("#wind");
     wind.append(" " + data.wind.speed + "MPH");
   });
-
+  //ajax for five day forecast
   $.ajax({
     url:
       "http://api.openweathermap.org/data/2.5/forecast/?q=" +
@@ -98,6 +89,7 @@ function setWeather() {
     method: "GET"
   }).then(function(data) {
     console.log(data);
+    //moment.js variable dates for next five days
     var dateOne = moment()
       .add(1, "days")
       .format("L");
@@ -114,17 +106,18 @@ function setWeather() {
       .add(5, "days")
       .format("L");
 
+    //variable array of dates
     var fiveDayWeekDates = [dateOne, dateTwo, dateThree, dateFour, dateFive];
 
     // console.log(fiveDayWeekDates);
-
+    //location dates will append to
     var dateTitle = document.getElementsByClassName("datetitle");
-
+    //for loop for dates
     for (var i = 0; i < fiveDayWeekDates.length; i++) {
       dateTitle[i].innerHTML = fiveDayWeekDates[i];
       //  console.log(fiveDayWeekDates.length);
     }
-
+    //temp array for five day forecast
     var array = [
       data.list[0].main.temp,
       data.list[1].main.temp,
@@ -136,13 +129,13 @@ function setWeather() {
     //console.log(array);
 
     var temperatureForecast = $(".tempFive");
-
+    //for floop for temp
     for (var i = 0; i < array.length; i++) {
       temperatureForecast[i].append("Temp:" + array[i] + "°F");
       // console.log(array.length);
       //console.log(temperatureForecast);
     }
-
+    //humidity array for five day forecast
     var humidArray = [
       data.list[0].main.humidity,
       data.list[1].main.humidity,
@@ -152,11 +145,11 @@ function setWeather() {
     ];
 
     var humidityForecast = $(".humidFive");
-
+    //for loop for humidity
     for (var i = 0; i < humidArray.length; i++) {
       humidityForecast[i].append("Humidity:" + humidArray[i] + "%");
     }
-
+    //added weather icons to five day forecast
     var weatherIcon1 = $("#weatherIcon1");
     var iconurl1 =
       "http://openweathermap.org/img/w/" +
